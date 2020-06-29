deb06b85e65781134a2cf911c6697e7d3756d8a6
YerinCho <forjustice9839@gmail.com>
2020-06-29 16:08:47 +0900

    feat: filter 선택시 버튼 테두리 변경되게 수정

--- src/TodoCount.js
+++ src/TodoCount.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import {FILTER_TYPE} from "./constants";
 
 const TodoCount = ({onFilter, count}) => {
+  const [selectAll, setSelectAll] = useState(true);
+  const [selectActive, setSelectActive] =  useState(false);
+  const [selectCompleted, setSelectCompleted] =  useState(false);
+
+
+  const setSelect = filterType => {
+    if(filterType === FILTER_TYPE.ALL) {
+      setSelectAll(true);
+      setSelectActive(false);
+      setSelectCompleted(false);
+    } else if (filterType === FILTER_TYPE.ACTIVE) {
+      setSelectAll(false);
+      setSelectActive(true);
+      setSelectCompleted(false);
+    } else if (filterType === FILTER_TYPE.COMPLETED) {
+      setSelectAll(false);
+      setSelectActive(false);
+      setSelectCompleted(true);
+    }
+  }
 
   return (
     <div className="count-container">
       <span className="todo-count">총 <strong>{count}</strong> 개</span>
       <ul className="filters">
         <li>
-          <a className="all selected" href="#/" onClick={() => onFilter(FILTER_TYPE.ALL)}>전체보기</a>
+          <a className={"all" + (selectAll ? " selected" : "")} href="#/" onClick={() => {
+            onFilter(FILTER_TYPE.ALL);
+            setSelect(FILTER_TYPE.ALL)
+          }}>전체보기</a>
         </li>
         <li>
-          <a className="active" href="#/active" onClick={() => onFilter(FILTER_TYPE.ACTIVE)}>해야할 일</a>
+          <a className={"active" + (selectActive ? " selected" : "")} href="#/active" onClick={() => {
+            onFilter(FILTER_TYPE.ACTIVE);
+            setSelect(FILTER_TYPE.ACTIVE)
+          }}>해야할 일</a>
         </li>
         <li>
-          <a className="completed" href="#/completed" onClick={() => onFilter(FILTER_TYPE.COMPLETED)}>완료한 일</a>
+          <a className={"completed" + (selectCompleted ? " selected" : "")} href="#/completed" onClick={() => {
+            onFilter(FILTER_TYPE.COMPLETED);
+            setSelect(FILTER_TYPE.COMPLETED)
+          }}>완료한 일</a>
         </li>
       </ul>
     </div>
